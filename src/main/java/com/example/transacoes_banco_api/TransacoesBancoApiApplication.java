4957ba2a39999c5a6d81f0c478ef1e9ac7007ce9
Bruno Almeida <brunoalmeida7426@gmail.com>
2025-03-09 22:49:49 +0000

    first commit

---
+++ src/main/java/com/example/transacoes_banco_api/TransacoesBancoApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.transacoes_banco_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransacoesBancoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransacoesBancoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/transacoes_banco_api/TransacoesBancoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.transacoes_banco_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacoesBancoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
